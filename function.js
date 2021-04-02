eced2e0d7eaa85e7b42512ae6a5eab0df91184b5
soregashi-27 <tomo.soul.15@gmail.com>
2021-04-02 15:17:09 +0900

    200steps

--- function.js
+++ function.js
@@ -138,3 +138,70 @@ function trueOrFalse(wasThatTrue) {
 }
 
 console.log(trueOrFalse(false));
+
+function testEqual(val) {
+  if (val == 12) {
+    return 'Equal';
+  }
+  return 'Not Equal';
+}
+
+console.log(testEqual(12));
+
+function testStrict(val) {
+  if (val == 7) {
+    return 'Equal';
+  }
+  return 'Not Equal';
+}
+
+console.log(testStrict(7));
+
+/* 
+== vs ===
+
+== : In the case of string-number comparison, converting string to number.
+
+=== : Absolutely the same.
+*/
+
+function compareEquality(a, b) {
+  if (a === b) {
+    return 'Equal';
+  }
+  return 'Not Equal';
+}
+
+console.log(compareEquality(10, '10'));
+
+function testNotEqual(val) {
+  if (val != 99) {
+    return 'Not Equal';
+  }
+  return 'Equal';
+}
+
+console.log(testNotEqual(199));
+
+function testStrictNotEqual(val) {
+  if (val !== 17) {
+    return 'Not Equal';
+  }
+  return 'Equal';
+}
+
+console.log(testStrictNotEqual(17));
+
+function testGreaterThan(val) {
+  if (val > 100) {
+    return 'Over 100';
+  }
+
+  if (val > 10) {
+    return 'Over 10';
+  }
+
+  return '10 or Under';
+}
+
+console.log(testGreaterThan(1000));
