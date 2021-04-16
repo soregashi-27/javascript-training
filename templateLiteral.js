c28ee3cddf5fa1294e72ee0cc70f4b5822d7f449
soregashi-27 <tomo.soul.15@gmail.com>
2021-04-16 15:37:49 +0900

    js-training

--- templateLiteral.js
+++ templateLiteral.js
@@ -1,1 +1,32 @@
 //テンプレートリテラル
+let name = 'Tom';
+let str = `Hello\n${name}`;
+console.log(str);
+
+//コード例
+// function b(strings, ...values) {
+//   console.log(strings);
+//   console.log(values);
+//   return strings.reduce((accu, str, i) => {
+//     let val = values[i] ? `<b>${values[i]}</b>` : '';
+//     return `${accu}${str}${val}`;
+//   }, '');
+// }
+
+function b(strings, ...values) {
+  console.log(strings);
+  console.log(values);
+  return strings.reduce((accu, str, i) => {
+    let val;
+    console.log(`${accu}${str}${val}`);
+    val = values[i] ? `<b>${values[i]}</b>` : '';
+    return `${accu}${str}${val}`;
+  }, '');
+}
+// 日本語では、変数の巻き上げ？と言うらしい https://bit.ly/3g9G4cO
+
+const str1 = 'Bob';
+const str2 = 'Tommy';
+const result = b`${str1} and ${str2}`;
+// const result = `${str1} and ${str2}`;
+console.log(result);
